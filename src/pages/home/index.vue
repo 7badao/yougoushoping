af973394715a472d0b51d52e87dd9c9a56620574
七把刀 <>
2019-12-02 11:48:19 +0800

    封装头部组件

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <!-- 头部搜索框 -->
-    <div class="header">
-      <div class="inputBox">
-        <icon type="search" size="20"></icon>
-        <span>搜索</span>
-      </div>
-    </div>
+    <searchLink />
     <!-- 轮播图 -->
     <swiper
       class="swiper"
@@ -49,31 +44,16 @@
 </template>
 
 <script>
-export default {}
+// 导入封装的头部组件
+import searchLink from '@/components/searchLink'
+export default {
+  components: {
+    searchLink
+  }
+}
 </script>
 
 <style lang="less">
-.header {
-  height: 100rpx;
-  padding: 0 16rpx;
-  display: flex;
-  align-items: center;
-  background-color: #eb4450;
-  .inputBox {
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-    height: 60rpx;
-    color: #bbb;
-    background-color: #fff;
-    font-size: 34rpx;
-    icon {
-      margin-right: 16rpx;
-    }
-  }
-}
 .swiper {
   height: 340rpx;
   swiper-item {
